a898fb2506eb939db449bc0234da5828d8e02f57
KishanJ22 <kishanjethwa224@gmail.com>
2024-09-03 20:11:31 +0100

    Add unit and router tests for circuits endpoint

--- src/circuits/circuits.core.test.ts
+++ src/circuits/circuits.core.test.ts
@@ -1,44 +1,95 @@
 import {getAllCircuits, getOneCircuit} from "./circuits.core";
 import { describe, expect, test, vi } from 'vitest';
-import * as circuitsModel from "./circuits.model";
+import prisma from "../__mocks__/prisma";
+import { Decimal } from "@prisma/client/runtime/library";
 
-const mockedCircuits = vi.spyOn(circuitsModel, "getCircuitsFromDb");
-describe ("/circuits GET Unit Tests", async () => {
-
-    test("Get all circuits", async () => {
-      mockedCircuits.mockResolvedValueOnce([
-        ["adelaide", "Adelaide", "Adelaide Street Circuit", null, "STREET", "Adelaide", "australia", "-34.927222", "138.617222", 11],
-        ["aida", "Aida", "Okayama International Circuit", "TI Circuit Aida", "RACE", "Aida", "japan", "34.915", "134.221111", 2],
-        ["ain-diab", "Ain-Diab", "Ain-Diab Circuit", null, "ROAD", "Casablanca", "morocco", "33.578611", "-7.6875", 1]
-      ]);
-      const circuits = await getAllCircuits();
+vi.mock('../db');
+describe ("Circuits Unit Tests", async () => {
+  describe("getAllCircuits", async () => {
+    test("Successfully returns all circuits in the correct format", async () => {
       const expectedCircuits = [
-        { id: "adelaide", name: "Adelaide", fullName: "Adelaide Street Circuit", previousNames: null, type: "STREET", placeName: "Adelaide", countryId: "australia", latitude: "-34.927222", longitude: "138.617222", racesHeld: 11 },
-        { id: "aida", name: "Aida", fullName: "Okayama International Circuit", previousNames: "TI Circuit Aida", type: "RACE", placeName: "Aida", countryId: "japan", latitude: "34.915", longitude: "134.221111", racesHeld: 2 },
-        { id: "ain-diab", name: "Ain-Diab", fullName: "Ain-Diab Circuit", previousNames: null, type: "ROAD", placeName: "Casablanca", countryId: "morocco", latitude: "33.578611", longitude: "-7.6875", racesHeld: 1 }
+        {
+          id: "adelaide",
+          name: "Adelaide",
+          full_name: "Adelaide Street Circuit",
+          previous_names: null,
+          type: "STREET",
+          place_name: "Adelaide",
+          country_id: "australia",
+          latitude: new Decimal(-34.927222),
+          longitude: new Decimal(138.617222),
+          total_races_held: 11
+        },
+        {
+          id: "aida",
+          name: "Aida",
+          full_name: "Okayama International Circuit",
+          previous_names: "TI Circuit Aida",
+          type: "RACE",
+          place_name: "Aida",
+          country_id: "japan",
+          latitude: new Decimal(34.915),
+          longitude: new Decimal(134.221111),
+          total_races_held: 2
+        },
       ];
+      prisma.circuit.findMany.mockResolvedValue(expectedCircuits);
+      const circuits = await getAllCircuits();
       expect(circuits).toEqual(expect.arrayContaining(expectedCircuits));
     });
 
-    test("Get one circuit", async () => {
-      mockedCircuits.mockResolvedValueOnce([
-        ["adelaide", "Adelaide", "Adelaide Street Circuit", null, "STREET", "Adelaide", "australia", "-34.927222", "138.617222", 11]
-      ]);
+    test("Successfully returns expected circuits after using a search query", async () => {
+      const expectedCircuits = [
+        {
+          id: "adelaide",
+          name: "Adelaide",
+          full_name: "Adelaide Street Circuit",
+          previous_names: null,
+          type: "STREET",
+          place_name: "Adelaide",
+          country_id: "australia",
+          latitude: new Decimal(-34.927222),
+          longitude: new Decimal(138.617222),
+          total_races_held: 11
+        }
+      ];
+      prisma.circuit.findMany.mockResolvedValue(expectedCircuits);
+      const full_name = "Adelaide Street Circuit";
+      const circuit = await getAllCircuits({ full_name });
+      expect(circuit).toEqual(expect.arrayContaining(expectedCircuits));
+    });
+
+    test("Successfully throws `No Circuits Found` error when an invalid search query is used", async () => {
+      await expect(getAllCircuits({ full_name: "Willow Springs" })).rejects.toThrow("No Circuits Found");
+    });
+
+    test("Successfully throws `Invalid Search Query` error when an invalid search query is used", async () => {
+      await expect(getAllCircuits({ invalid_key: "Adelaide Street Circuit" })).rejects.toThrow("Invalid Search Query");
+    });
+  });
+
+  describe("getOneCircuit", async () => {
+    test("Successfully returns the expected circuit when provided an id", async () => {
+      const expectedCircuit =
+      {
+        id: "adelaide",
+        name: "Adelaide",
+        full_name: "Adelaide Street Circuit",
+        previous_names: null,
+        type: "STREET",
+        place_name: "Adelaide",
+        country_id: "australia",
+        latitude: new Decimal(-34.927222),
+        longitude: new Decimal(138.617222),
+        total_races_held: 11
+      };
+      prisma.circuit.findUnique.mockResolvedValue(expectedCircuit);
       const circuit = await getOneCircuit("adelaide");
-      const expectedCircuit = { id: "adelaide", name: "Adelaide", fullName: "Adelaide Street Circuit", previousNames: null, type: "STREET", placeName: "Adelaide", countryId: "australia", latitude: "-34.927222", longitude: "138.617222", racesHeld: 11 };
       expect(circuit).toEqual(expectedCircuit);
     });
 
-    test("Error is thrown when circuit is not found", async () => {
+    test("Successfully throws `No Circuit Found` error when an invalid circuit id is used", async () => {
       await expect(getOneCircuit("willow-springs")).rejects.toThrow("No Circuit Found");
     });
-
-    test("Get all circuits with search query", async () => {
-      mockedCircuits.mockResolvedValueOnce([
-        ["adelaide", "Adelaide", "Adelaide Street Circuit", null, "STREET", "Adelaide", "australia", "-34.927222", "138.617222", 11]
-      ]);
-      const circuit = await getAllCircuits({fullName: "Adelaide Street Circuit"});
-      const expectedCircuit = { id: "adelaide", name: "Adelaide", fullName: "Adelaide Street Circuit", previousNames: null, type: "STREET", placeName: "Adelaide", countryId: "australia", latitude: "-34.927222", longitude: "138.617222", racesHeld: 11 };
-      expect(circuit).toEqual(expect.arrayContaining([expectedCircuit]));
-    });
+  });
 });
