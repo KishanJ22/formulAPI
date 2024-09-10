092d8ecdc0be5abffe435141749afa8179fff1cd
KishanJ22 <kishanjethwa224@gmail.com>
2024-09-10 19:11:24 +0100

    Fix root endpoint and /health endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-import express, { Response } from "express";
+import express, { Request, Response } from "express";
 import { driverRouter } from "./drivers/drivers.router";
 import { circuitRouter } from "./circuits/circuits.router";
 import { PORT } from "./config";
 
 const app = express();
 
-app.get("/", (res: Response) => {
+app.get("/", (req: Request, res: Response) => {
     res.send({ message: "Welcome to FormulAPI!" });
 });
 
-app.get("/health", (res: Response) => {
+app.get("/health", (req: Request, res: Response) => {
     res.send({
         message: "Ready to receive requests",
         version: `${process.env.npm_package_version}`,
