96cf6d851e4e065923d6256e279973025746ff5a
KishanJ22 <kishanjethwa224@gmail.com>
2024-09-15 17:51:33 +0100

    Setup linter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,25 @@
 import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
+import tseslint from 'typescript-eslint'
 import eslintConfigPrettier from "eslint-config-prettier";
+import globals from 'globals';
 
 export default [
     eslint.configs.recommended,
     ...tseslint.configs.strict,
     ...tseslint.configs.stylistic,
-    eslintConfigPrettier,
     {
-        ignores: [
-            ".eslintrc.js",
-            "eslint.config.mjs",
-            ".node_modules/",
-            ".dist/",
-        ],
+        languageOptions: {
+            globals: {
+                ...globals.node,
+            },
+        },
         rules: {
+
             "@typescript-eslint/no-unused-vars": "warn",
             "@typescript-eslint/no-require-imports": "warn",
             "@typescript-eslint/no-explicit-any": "off",
-            "no-undef": "off",
+            "no-unused-vars": "off",
         },
     },
+    eslintConfigPrettier,
 ];
