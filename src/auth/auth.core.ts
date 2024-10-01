78bfb74746843926a1f353a577cb7a563f6727cc
KishanJ22 <kishanjethwa224@gmail.com>
2024-10-01 23:10:30 +0100

    Update authenticateUser function and add JWT generator

--- src/auth/auth.core.ts
+++ src/auth/auth.core.ts
@@ -1,12 +1,44 @@
-import { getAuthSecret } from "./auth.model";
+import { getAuthSecret, getSingleUser } from "./auth.model";
+import { sign } from "jsonwebtoken";
 
-export const getSecret = async (username: string, key: string): Promise<boolean> => {
+export const authenticateUser = async (
+    username: string,
+    key: string,
+): Promise<string | false> => {
+    const user = await getSingleUser(username);
+    if (!user) {
+        return false;
+    }
     const secrets = await getAuthSecret(username);
     const userSecret = secrets.find((secret: any) => secret.key === key);
-
     if (!userSecret) {
         return false;
     }
+    return generateJwt(username, key);
+};
 
-    return true;
+export const generateJwt = async (
+    username: string,
+    key: string,
+): Promise<string | false> => {
+    const user = await getSingleUser(username);
+    if (!user) {
+        return false;
+    }
+    const token = sign(
+        {
+            username: user.username,
+            first_name: user.first_name,
+            last_name: user.last_name,
+            role: user.role,
+        },
+        key,
+        {
+            subject: user.username as string,
+            algorithm: "HS256",
+            issuer: "auth-service",
+            expiresIn: "1h",
+        },
+    );
+    return token;
 };
