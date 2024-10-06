5a3c7a2422f6e4745dbf25ddb6f382fa7e8c2fae
KishanJ22 <kishanjethwa224@gmail.com>
2024-10-06 16:08:00 +0100

    Add auth/register endpoint and update API key getter

--- src/auth/auth.core.ts
+++ src/auth/auth.core.ts
@@ -1,18 +1,36 @@
 import { NextFunction, Request, Response } from "express";
-import { getAuthSecret, getSingleUser } from "./auth.model";
+import { createUser, getUserKey, getSingleUser, getUsers } from "./auth.model";
 import { sign, verify, JwtPayload } from "jsonwebtoken";
 import { setAsyncLocalStorage } from "../plugins/local-storage";
 
 const jwtSecret = process.env.JWT_SECRET ?? "";
 
-export const authenticateUser = async (username: string, key: string) => {
+export const registerUser = async (
+    username: string,
+    first_name: string,
+    last_name: string,
+) => {
+    const users = await getUsers();
+    let userId = crypto.randomUUID();
+    const userIdExists = users.find((user: any) => user.id === userId);
+    if (await getSingleUser(username)) {
+        return false;
+    }
+    if (userIdExists) {
+        userId = crypto.randomUUID();
+    }
+    await createUser(userId, username, first_name, last_name);
+    const api_key = await getUserKey(username);
+    return { message: "User created", username, api_key };
+};
+
+export const authenticateUser = async (username: string) => {
     const user = await getSingleUser(username);
     if (!user) {
         return false;
     }
-    const secrets = await getAuthSecret(username);
-    const userSecret = secrets.find((secret: any) => secret.key === key);
-    if (!userSecret) {
+    const secret = await getUserKey(username);
+    if (!secret) {
         return false;
     }
     return generateJwt(username);
--- src/auth/auth.router.ts
+++ src/auth/auth.router.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response, Router } from "express";
-import { authenticateUser, verifyJwt } from "./auth.core";
+import { authenticateUser, verifyJwt, registerUser } from "./auth.core";
 import { getAsyncLocalStorage } from "../plugins/local-storage";
 import { getUsers } from "./auth.model";
 
@@ -8,9 +8,18 @@ export const authRouter = (app: Router) => {
     const base = "/auth";
     app.use(base, router);
 
+    router.post("/register", async (req: Request, res: Response) => {
+        const { username, first_name, last_name } = req.body;
+        const user = await registerUser(username, first_name, last_name);
+        if (user) {
+            return res.status(201).send(user);
+        }
+        return res.status(400).send({ message: "Failed to create user" });
+    });
+
     router.post("/get-token", async (req: Request, res: Response) => {
-        const { username, key } = req.body;
-        const authToken = await authenticateUser(username, key);
+        const { username } = req.body;
+        const authToken = await authenticateUser(username);
         if (authToken) {
             return res.status(200).send({ authToken });
         } else {
@@ -31,9 +40,14 @@ export const authRouter = (app: Router) => {
         "/verify-token",
         verifyJwt,
         async (req: Request, res: Response) => {
-            return res
-                .status(200)
-                .send({ message: "Token is valid" });
+            const store = getAsyncLocalStorage();
+            try {
+                return res
+                    .status(200)
+                    .send({ message: "Token is valid", userDetails: store });
+            } catch {
+                return res.status(401).send({ message: "Token is invalid" });
+            }
         },
     );
 };
