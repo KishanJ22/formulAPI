a197bd29935c11661036828aae71a118a269c7ee
KishanJ22 <kishanjethwa224@gmail.com>
2024-11-01 21:02:26 +0000

    Add pino logger and some fastify plugins

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,19 @@
-import { PORT as port } from "./config";
+import { PORT as port, logger } from "./config";
 import fastify, { FastifyInstance } from "fastify";
 
 const app: FastifyInstance = fastify({
-    logger: true,
+    logger,
 });
 
-app.get("/", async (request, reply) => {
-    reply.send({ message: "Welcome to FormulAPI!" });
-});
-
-app.listen({ port }, function (err, address) {
+app.listen({ port }, function (err) {
     if (err) {
         app.log.error(err);
         process.exit(1);
     }
-    app.log.info(`server listening on ${address}`);
+});
+
+app.get("/", async (request, reply) => {
+    reply.status(200).send({ message: "Welcome to FormulAPI!" });
 });
 
 export default app;
--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,15 @@ import { PrismaClient } from "@prisma/client";
 
 export const PORT = Number(process.env.PORT ?? 3000);
 export const APP_URL = process.env.APP_URL;
-export const prisma = new PrismaClient(); 
+export const prisma = new PrismaClient();
+
+export const logger = {
+    transport: {
+        target: "pino-pretty",
+        options: {
+            translateTime: "HH:MM:ss Z",
+            ignore: "pid,hostname",
+            
+        },
+    },
+};
