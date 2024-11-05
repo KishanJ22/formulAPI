26aa85fce7137d7784d0395b8255109e60311289
KishanJ22 <kishanjethwa224@gmail.com>
2024-11-05 23:36:49 +0000

    Refactor circuits

--- src/routes/circuits/circuits.get.ts
+++ src/routes/circuits/circuits.get.ts
@@ -12,6 +12,7 @@ type NotFoundSchema = Static<typeof notFound>;
 
 const getCircuits = (fastify: FastifyInstance) => {
     fastify.get<{
+        QueryString: Circuit;
         Reply:
             | { data: Circuit[] }
             | NotFoundSchema
@@ -21,6 +22,10 @@ const getCircuits = (fastify: FastifyInstance) => {
         "/circuits",
         {
             schema: {
+                querystring: {
+                    type: "object",
+                    properties: circuit.properties,
+                },
                 response: {
                     200: Type.Object({ data: Type.Array(circuit) }),
                     400: invalidQuery,
@@ -33,17 +38,36 @@ const getCircuits = (fastify: FastifyInstance) => {
             try {
                 const circuits = await prisma.circuit.findMany();
 
-                if (circuits.length === 0) {
-                    return reply
-                        .status(404)
-                        .send({ message: "No Circuits Found" });
-                }
-
                 const formattedCircuits: Circuit[] = circuits.map((circuit) => ({
                     ...circuit,
                     latitude: circuit.latitude.toNumber(),
                     longitude: circuit.longitude.toNumber(),
                 }));
+
+                if (request.query && Object.keys(request.query).length > 0) {
+                    const query: Partial<Circuit> = request.query;
+                    const queryKeys = Object.keys(query) as (keyof Circuit)[];
+
+                    if (
+                        !queryKeys.every((key) =>
+                            Object.keys(circuit.properties).includes(key),
+                        )
+                    ) {
+                        return reply.status(400).send({ message: "Invalid Search Query" });
+                    }
+
+                    const filteredCircuits = formattedCircuits.filter((circuit) =>
+                        queryKeys.every(
+                            (key) => circuit[key] === query[key],
+                        ),
+                    );
+
+                    if (filteredCircuits.length === 0) {
+                        return reply.status(404).send({ message: "No Circuits Found" });
+                    }
+
+                    return reply.status(200).send({ data: filteredCircuits });
+                }
 
                 return reply.status(200).send({ data: formattedCircuits });
             } catch (error) {
