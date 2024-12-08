40bd985b4de258f887fc6a145f0355b0bd4b7acb
KishanJ22 <kishanjethwa224@gmail.com>
2024-12-08 21:28:17 +0000

    Move root and health endpoints to separate files

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,6 @@ import fastifyAutoload from "@fastify/autoload";
 import { dirname, join } from "path";
 import { fileURLToPath } from "url";
 import registerRoutes from "./routes/routes.js";
-import { prisma } from "./config.js";
 
 const app: FastifyInstance = fastify({
     logger,
@@ -24,19 +23,6 @@ app.register(registerRoutes);
 app.register(FastifyFormBody);
 app.register(fastifyJwt, {
     secret: app.config.JWT_SECRET,
-});
-
-app.get("/", async (request, reply) => {
-    return { message: "Welcome to FormulAPI!" };
-});
-
-app.get("/health", async (request, reply) => {
-    const getData = await prisma.driver.findFirst();
-    if (getData) {
-        return reply.status(200).send({ message: "API is healthy" });
-    } else {
-        return reply.status(500).send({ message: "API is unhealthy" });
-    }
 });
 
 export const loadApp = async () => {
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,10 +1,14 @@
 import { FastifyInstance } from "fastify";
+import getRoot from "./getRoot.js";
+import getHealth from "./health.js";
 import getDrivers from "./drivers/drivers.get.js";
 import getCircuits from "./circuits/circuits.get.js";
 import getMetrics from "./metrics.js";
 import getConstructors from "./constructors/constructors.get.js";
 
 const registerRoutes = async (fastify: FastifyInstance) => {
+    fastify.register(getRoot);
+    fastify.register(getHealth);
     fastify.register(getDrivers);
     fastify.register(getCircuits);
     fastify.register(getConstructors);
