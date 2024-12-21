6899904c4a350a4fa2f5b8d02646277ef2563378
KishanJ22 <kishanjethwa224@gmail.com>
2024-12-21 16:44:45 +0000

    Update config

--- src/config.ts
+++ src/config.ts
@@ -12,6 +12,46 @@ export const logger = {
     },
 };
 
+export const openapi = {
+    info: {
+        title: "FormulAPI",
+        version: "1.0.0",
+        description: "API for getting Formula One data",
+    },
+    servers: [
+        {
+            url: "http://localhost:3000",
+            description: "Local server",
+        },
+        {
+            url: "https://formulapi-dev.kjet.dev:3000",
+            description: "Development server",
+        },
+    ],
+    tags: [
+        {
+            name: "drivers",
+            description: "Driver related endpoints",
+        },
+        {
+            name: "constructors",
+            description: "Constructor related endpoints",
+        },
+        {
+            name: "circuits",
+            description: "Circuit related endpoints",
+        },
+        {
+            name: "races",
+            description: "Race related endpoints",
+        },
+        {
+            name: "grand-prix",
+            description: "Grand Prix related endpoints",
+        },
+    ],
+};
+
 export const envOptions = {
     dotenv: true,
     schema: {
@@ -31,6 +71,10 @@ export const envOptions = {
             DATABASE_URL: {
                 type: "string",
             },
+            NODE_ENV: {
+                type: "string",
+                default: "development",
+            }
         },
     },
 };
